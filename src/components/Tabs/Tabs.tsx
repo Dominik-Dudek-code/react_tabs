31b959cc5fd6e35ea143572b78a750b778305280
Dominik Dudek <dom-dud774@o2.pl>
2025-01-31 13:30:20 +0100

    Task solution

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,1 +1,25 @@
-export const Tabs = () => {};
+import { Tab } from '../../App';
+
+type TabProps = {
+  tab: Tab;
+  selectedTab: Tab;
+  onTabSelected: (tab: Tab, event: React.MouseEvent<HTMLAnchorElement>) => void;
+};
+
+export const Tabs: React.FC<TabProps> = ({
+  tab,
+  selectedTab,
+  onTabSelected,
+}) => {
+  return (
+    <li className={tab.id === selectedTab.id ? 'is-active' : ''} data-cy="Tab">
+      <a
+        href={`#${tab.id}`}
+        data-cy="TabLink"
+        onClick={event => onTabSelected(tab, event)}
+      >
+        {tab.title}
+      </a>
+    </li>
+  );
+};
